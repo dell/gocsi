a2e1a90d9bea4fd4f417a8b35053472877730133
Christian Coffield <christian_coffield@dell.com>
2025-01-22 16:41:38 -0500

    create volume + create snapshot tests

--- csc/cmd/controller_test.go
+++ csc/cmd/controller_test.go
@@ -1,8 +1,15 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"text/template"
+	"time"
 
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/dell/gocsi/mock/service"
+	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,18 +20,81 @@ func TestControllerCmd(t *testing.T) {
 	err := child.PersistentPreRunE(child, []string{})
 	assert.NoError(t, err)
 
-	// TODO: Add test case for error condition in PreRun
-	// The only way to do that is to trigger an error in root command's
-	// prerun, and I don't see any args that can be passed to it that would cause such a thing
-	// Very high effort for one line of coverage.
+	// test case: error
+	// force RootCmd to return error
+	RootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		return fmt.Errorf("test error")
+	}
+	err = child.PersistentPreRunE(child, []string{})
+	assert.Error(t, err)
 
 }
 
 func TestCreateSnapshotCmd(t *testing.T) {
 	child := createSnapshotCmd
+	// set up root as required
+	root.ctx = context.Background()
+	root.timeout = 10 * time.Second
+	tpl, err := template.New("t").Funcs(template.FuncMap{
+		"isa": func(o interface{}, t string) bool {
+			return fmt.Sprintf("%T", o) == t
+		},
+	}).Parse(root.format)
+	assert.NoError(t, err)
+	root.tpl = tpl
 
-	// TODO: Valid test cases
-	child.RunE(RootCmd, []string{"name"})
+	// set up the CSI client with a mock
+	controller.client = service.NewClient()
 
-	// TODO: Add test case for error condition in RunE
+	// Valid test case
+	createSnapshot.sourceVol = "Mock Volume 1"
+	err = child.RunE(RootCmd, []string{"testname"})
+	assert.NoError(t, err)
+
+	// error test case - empty sourceVol
+	createSnapshot.sourceVol = ""
+	err = child.RunE(RootCmd, []string{"testname"})
+	assert.Error(t, err)
+
+	// TODO: CreateSnapshot error case, tpl.Execute() error case
+}
+
+func TestCreateVolumeCmd(t *testing.T) {
+	child := createVolumeCmd
+	// set up root as required
+	root.ctx = context.Background()
+	root.timeout = 10 * time.Second
+	tpl, err := template.New("t").Funcs(template.FuncMap{
+		"isa": func(o interface{}, t string) bool {
+			return fmt.Sprintf("%T", o) == t
+		},
+	}).Parse(root.format)
+	assert.NoError(t, err)
+	root.tpl = tpl
+
+	// set up the CSI client with a mock
+	controller.client = service.NewClient()
+
+	// Valid test case
+	createVolume.reqBytes = 100
+	createVolume.limBytes = 200
+	createVolume.caps = volumeCapabilitySliceArg{data: []*csi.VolumeCapability{{AccessMode: &csi.VolumeCapability_AccessMode{Mode: csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER}}}}
+	createVolume.params = mapOfStringArg{data: map[string]string{"key1": "value1", "key2": "value2"}}
+	createVolume.sourceVol = "source-volume"
+	createVolume.sourceSnap = ""
+	err = child.RunE(RootCmd, []string{"testname"})
+	assert.NoError(t, err)
+
+	// Valid test case 2: snapshot
+	createVolume.sourceVol = ""
+	createVolume.sourceSnap = "source-snap"
+	err = child.RunE(RootCmd, []string{"testname"})
+	assert.NoError(t, err)
+
+	// Error test case: have both source vol and source snap
+	createVolume.sourceVol = "source-volume"
+	err = child.RunE(RootCmd, []string{"testname"})
+	assert.Error(t, err)
+
+	// TODO: more error test cases
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/pflag v1.0.5
+	github.com/stretchr/testify v1.10.0
+	go.etcd.io/etcd/client/pkg/v3 v3.5.17
 	go.etcd.io/etcd/client/v3 v3.5.17
 	go.etcd.io/etcd/server/v3 v3.5.17
 	golang.org/x/net v0.34.0
@@ -24,6 +26,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/coreos/go-semver v0.3.1 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
@@ -46,18 +49,17 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/nxadm/tail v1.4.11 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.11.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.26.0 // indirect
 	github.com/prometheus/procfs v0.6.0 // indirect
 	github.com/soheilhy/cmux v0.1.5 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/thecodeteam/gosync v0.1.0 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20201229170055-e5319fda7802 // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.etcd.io/bbolt v1.3.11 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.17 // indirect
-	go.etcd.io/etcd/client/pkg/v3 v3.5.17 // indirect
 	go.etcd.io/etcd/client/v2 v2.305.17 // indirect
 	go.etcd.io/etcd/pkg/v3 v3.5.17 // indirect
 	go.etcd.io/etcd/raft/v3 v3.5.17 // indirect
