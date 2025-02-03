6e8d3a5216eb3fdc29962bcf651481f1bfe0dac3
JacobGros <jacobgrosner4@gmail.com>
2025-02-03 12:00:09 -0500

    add testing for volume_lock_provider.go

---
+++ middleware/serialvolume/types/volume_lock_provider_test.go
@@ -0,0 +1,44 @@
+package types
+
+import (
+	"context"
+	"testing"
+
+	"github.com/akutz/gosync"
+	"github.com/stretchr/testify/assert"
+)
+
+// MyType implements the VolumeLockerProvider interface
+type MyType struct{}
+
+// Methods for MyType to implement the VolumeLockerProvider interface
+func (ml *MyType) GetLockWithID(ctx context.Context, id string) (gosync.TryLocker, error) {
+	lock := &gosync.TryMutex{}
+	return lock, nil
+}
+
+func (ml *MyType) GetLockWithName(ctx context.Context, name string) (gosync.TryLocker, error) {
+	lock := &gosync.TryMutex{}
+	return lock, nil
+}
+
+// Method to test the interface methods defined above
+func testInterfaceMethods(l VolumeLockerProvider) error {
+	ctx := context.Background()
+
+	_, err := l.GetLockWithID(ctx, "testId")
+	if err != nil {
+		return err
+	}
+	_, err = l.GetLockWithName(ctx, "testName")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestVolumeLockerProvider(t *testing.T) {
+	myType := &MyType{}
+	err := testInterfaceMethods(myType)
+	assert.NoError(t, err)
+}
