b793679a49d1777365d190385ce4a25f98c63d78
JacobGros <jacobgrosner4@gmail.com>
2025-02-03 08:49:35 -0500

    add coverage to csc/main.go

--- csc/main_test.go
+++ csc/main_test.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(_ *testing.M) {
-	main()
+func TestMain(t *testing.T) {
+	// main() does not return any errors or values, so verify panic wasn't hit
+	assert.NotPanics(t, func() { main() })
 }
