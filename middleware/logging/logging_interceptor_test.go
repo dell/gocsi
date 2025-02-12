e4d37a13a56a38b6af496135c6b059fa33ad28d7
alexemc <32580729+alexemc@users.noreply.github.com>
2025-02-12 13:22:20 -0500

    logging package test coverage increased to 100%

--- middleware/logging/logging_interceptor_test.go
+++ middleware/logging/logging_interceptor_test.go
@@ -4,127 +4,54 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"io"
-	"os"
 	"testing"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	csictx "github.com/dell/gocsi/context"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
 
-func TestWithRequestLogging(t *testing.T) {
-	w := &bytes.Buffer{}
-	tests := []struct {
-		name  string
-		input io.Writer
-		want  opts
-	}{
-		{
-			name: "non nil input",
-			want: opts{
-				reqw:             w,
-				repw:             nil,
-				disableLogVolCtx: false,
-			},
-			input: w,
-		},
-		{
-			name: "nil input",
-			want: opts{
-				reqw:             os.Stdout,
-				repw:             nil,
-				disableLogVolCtx: false,
-			},
-			input: nil,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			opt := WithRequestLogging(tt.input)
+func TestClientLogger(t *testing.T) {
+	cLogger := NewClientLogger()
 
-			// Use interceptor type to check the expected opts
-			i := newLoggingInterceptor(opt)
-
-			// Check interceptor type opts to verify WithRequestLogging set them as expected
-			if i.opts != tt.want {
-				t.Errorf("WithRequestLogging() returned function with parameters = %v, want function with parameters %v", i.opts, tt.want)
-			}
-		})
+	invoker := func(
+		ctx context.Context,
+		method string,
+		req, rep interface{},
+		cc *grpc.ClientConn,
+		opts ...grpc.CallOption,
+	) error {
+		return nil
 	}
+
+	err := cLogger(
+		context.Background(),
+		"TestMethod",
+		&csi.CreateVolumeRequest{},
+		&csi.CreateVolumeResponse{},
+		&grpc.ClientConn{},
+		invoker,
+		grpc.EmptyCallOption{},
+	)
+	assert.NoError(t, err)
 }
 
-func TestWithResponseLogging(t *testing.T) {
-	w := &bytes.Buffer{}
-	tests := []struct {
-		name  string
-		input io.Writer
-		want  opts
-	}{
-		{
-			name: "non nil input",
-			want: opts{
-				reqw:             nil,
-				repw:             w,
-				disableLogVolCtx: false,
-			},
-			input: w,
-		},
-		{
-			name: "nil input",
-			want: opts{
-				reqw:             nil,
-				repw:             os.Stdout,
-				disableLogVolCtx: false,
-			},
-			input: nil,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			opt := WithResponseLogging(tt.input)
+func TestServerLogger(t *testing.T) {
+	sLogger := NewServerLogger()
 
-			// Use interceptor type to check the expected opts
-			i := newLoggingInterceptor(opt)
-
-			// Check interceptor type opts to verify WithResponseLogging set them as expected
-			if i.opts != tt.want {
-				t.Errorf("WithResponseLogging() returned function with parameters = %v, want function with parameters %v", i.opts, tt.want)
-			}
-		})
-	}
-}
-
-func TestWithDisableLogVolumeContext(t *testing.T) {
-	tests := []struct {
-		name string
-		want bool
-	}{
-		{
-			name: "switch to true",
-			want: true,
+	res, err := sLogger(
+		context.Background(),
+		&csi.CreateVolumeRequest{},
+		&grpc.UnaryServerInfo{},
+		// grpc.UnaryHandler handler
+		func(ctx context.Context, req interface{}) (interface{}, error) {
+			return &csi.CreateVolumeResponse{}, nil
 		},
-		{
-			name: "test default",
-			want: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// Use interceptor type to check if WithDisableLogVolumeContext is able to switch value from false to true
-			i := newLoggingInterceptor()
-
-			// if want = true, use WithDisableLogVolumeContext to switch disableLogVolCtx from false to true
-			if tt.want {
-				i = newLoggingInterceptor(WithDisableLogVolumeContext())
-			}
-			// Check interceptor type opts to verify WithDisableLogVolumeContext switched or didn't switch
-			if i.opts.disableLogVolCtx != tt.want {
-				t.Errorf("WithResponseLogging() returned function with parameters = %v, want function with parameters %v", i.opts, tt.want)
-			}
-		})
-	}
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, &csi.CreateVolumeResponse{}, res)
 }
 
 func TestHandle(t *testing.T) {
@@ -205,8 +132,17 @@ func TestHandle(t *testing.T) {
 		},
 		{
 			name: "with request and response logging",
-			i:    newLoggingInterceptor(WithRequestLogging(w), WithResponseLogging(w)),
-			req:  defaultReq,
+			i: newLoggingInterceptor(WithRequestLogging(nil),
+				WithResponseLogging(nil), WithDisableLogVolumeContext()),
+			// special object type to trigger all checks
+			req: &struct {
+				// field called "Secrets"
+				Secrets string
+				// field called "VolumeContext"
+				VolumeContext struct{}
+				// unexported field from another package
+				md metadata.MD
+			}{},
 			next: defaultNext,
 			getCtx: func() context.Context {
 				md := metadata.Pairs(
